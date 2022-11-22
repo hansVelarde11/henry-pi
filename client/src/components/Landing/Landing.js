c7799ab94a6ee35b73d8d202b4e00e7d246a652b
hansVelarde11 <hsssvf@gmail.com>
2022-11-22 00:24:34 -0500

    check

---
+++ client/src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import {React } from 'react'
+import './landing.css'
+
+export default function Landing() {
+
+  return (
+    <div className="MyImage">
+        <img className="theImage" src="" alt="" />
+        <Link to="/videogames">
+          <button className="myButton">INSERT COIN</button>
+        </Link>
+    </div>
+  );
+}
