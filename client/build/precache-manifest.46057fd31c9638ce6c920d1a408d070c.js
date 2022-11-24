4ee0441de5fc6c683b06e03e2cbc2f31874a6864
hansVelarde11 <hsssvf@gmail.com>
2022-11-23 23:03:58 -0500

    check

---
+++ client/build/precache-manifest.46057fd31c9638ce6c920d1a408d070c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9bd60c035ffddf591b3",
+    "url": "/static/css/main.ca19313a.chunk.css"
+  },
+  {
+    "revision": "c9bd60c035ffddf591b3",
+    "url": "/static/js/main.c9bd60c0.chunk.js"
+  },
+  {
+    "revision": "1d7f6937754cce323376",
+    "url": "/static/js/1.1d7f6937.chunk.js"
+  },
+  {
+    "revision": "e2816469d0b5a21c1adc",
+    "url": "/static/js/2.e2816469.chunk.js"
+  },
+  {
+    "revision": "270189256389afc04cea",
+    "url": "/static/js/runtime~main.27018925.js"
+  },
+  {
+    "revision": "7028e8e32483ae274b9dc430904677ea",
+    "url": "/static/media/created.7028e8e3.jpg"
+  },
+  {
+    "revision": "46d0f6b5fba77fb4941fd25c380010a0",
+    "url": "/static/media/about.46d0f6b5.gif"
+  },
+  {
+    "revision": "6f3a3588e368613ae0769bbfc9aee215",
+    "url": "/static/media/CodePen-404-Page.6f3a3588.gif"
+  },
+  {
+    "revision": "b817094dabb0d7ffefe4aa8a0a196f85",
+    "url": "/static/media/llorando.b817094d.png"
+  },
+  {
+    "revision": "4260cb6eef103e3e55990dd1e3a62db8",
+    "url": "/static/media/conecting.4260cb6e.gif"
+  },
+  {
+    "revision": "e302aaa8984698bba505afaf2fe67581",
+    "url": "/static/media/landing.e302aaa8.jpg"
+  },
+  {
+    "revision": "70cd4522bca2d01f9e8e9c2d54e1775b",
+    "url": "/index.html"
+  }
+];
